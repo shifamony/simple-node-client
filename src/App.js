0170661b1b498043d456165445a1eb62d4ea0ba4
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-10-28 22:55:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+ 
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name,email);
+    form.reset();
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" name='name' />
+        <input type="email" name='email' />
+        <button>Submit</button>
+      </form>
+     <h1>users: {users.length}</h1>
+     <div>
+    {
+        users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+    }
+     </div>
     </div>
   );
 }
