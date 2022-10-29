6853be5035415afb78e6511c35eac7b41562d4b7
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-10-29 20:05:51 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -16,22 +16,46 @@ function App() {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    console.log(name,email);
+    const user = {name,email}
+    console.log(user);
+    fetch('http://localhost:5000/users',{
+      method: 'POST', // or 'PUT'
+
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify(user),
+      })
+
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Success:', data);
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
     form.reset();
+
   }
 
 
   return (
     <div className="App">
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" name='name' />
-        <input type="email" name='email' />
-        <button>Submit</button>
+        <br /><br />
+        <input type="text" name='name' placeholder='Your Name' />
+        <br /><br />
+        <input type="email" name='email' placeholder='Your email' />
+        <br /><br />
+        <button>Add User</button>
       </form>
      <h1>users: {users.length}</h1>
      <div>
     {
-        users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        users.map(user => <p key={user._id}>{user.name} {user.email}</p>)
     }
      </div>
     </div>
